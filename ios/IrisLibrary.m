0ac829de411a800d41173d269f7bf08db94e83f7
Thiago R C Brandt <thiago.brandt@4all.com>
2020-03-17 18:05:30 -0300

    remove parameter from RCT_EXTERN_METHOD bridge

--- ios/IrisLibrary.m
+++ ios/IrisLibrary.m
@@ -1,8 +1,8 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(IrisLibrary, NSObject)
-RCT_EXTERN_METHOD(initNotifications:(NSDictionary * _Nullable)launchOptions)
-RCT_EXTERN_METHOD(initGeolocation:(NSDictionary * _Nullable)launchOptions)
+RCT_EXTERN_METHOD(initNotifications)
+RCT_EXTERN_METHOD(initGeolocation)
 RCT_EXTERN_METHOD(sendTag:(NSString *)key value:(NSString *)value)
 @end
 
