d90b1c9920e503c2c5db7a6f6dfb9124647ad22c
Thiago Brandt <thiagobrandt@IRIS-Thiagos-Mac-mini.local>
2020-10-07 15:16:23 -0300

    added exception handler for utils class

--- android/src/main/java/com/reactlibrary/ReactNativeJson.java
+++ android/src/main/java/com/reactlibrary/ReactNativeJson.java
@@ -15,62 +15,96 @@ import org.json.JSONObject;
 import java.util.Iterator;
 
 public class ReactNativeJson {
-    public static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+    public static WritableMap convertJsonToMap(JSONObject jsonObject){
         WritableMap map = new WritableNativeMap();
 
+        if (jsonObject == null) {
+            return null;
+        }
+
         Iterator<String> iterator = jsonObject.keys();
+        if (!iterator.hasNext()) {
+            return null;
+        }
+
         while (iterator.hasNext()) {
             String key = iterator.next();
-            Object value = jsonObject.get(key);
-            if (value instanceof JSONObject) {
-                map.putMap(key, convertJsonToMap((JSONObject) value));
-            } else if (value instanceof JSONArray) {
-                map.putArray(key, convertJsonToArray((JSONArray) value));
-            } else if (value instanceof  Boolean) {
-                map.putBoolean(key, (Boolean) value);
-            } else if (value instanceof  Integer) {
-                map.putInt(key, (Integer) value);
-            } else if (value instanceof  Double) {
-                map.putDouble(key, (Double) value);
-            } else if (value instanceof String)  {
-                map.putString(key, (String) value);
-            } else {
-                map.putString(key, value.toString());
+
+            try {
+                Object value = jsonObject.get(key);
+                if (value instanceof JSONObject) {
+                    map.putMap(key, convertJsonToMap((JSONObject) value));
+                } else if (value instanceof JSONArray) {
+                    map.putArray(key, convertJsonToArray((JSONArray) value));
+                } else if (value instanceof  Boolean) {
+                    map.putBoolean(key, (Boolean) value);
+                } else if (value instanceof  Integer) {
+                    map.putInt(key, (Integer) value);
+                } else if (value instanceof  Double) {
+                    map.putDouble(key, (Double) value);
+                } else if (value instanceof String)  {
+                    map.putString(key, (String) value);
+                } else {
+                    map.putString(key, value.toString());
+                }
+            } catch(JSONException e) {
+
             }
         }
         return map;
     }
 
-    public static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
+    public static WritableArray convertJsonToArray(JSONArray jsonArray){
         WritableArray array = new WritableNativeArray();
+        
+        if (jsonArray == null) {
+            return null;
+        }
+
+        if (jsonArray.length() <= 0) {
+            return null;
+        }
 
         for (int i = 0; i < jsonArray.length(); i++) {
-            Object value = jsonArray.get(i);
-            if (value instanceof JSONObject) {
-                array.pushMap(convertJsonToMap((JSONObject) value));
-            } else if (value instanceof  JSONArray) {
-                array.pushArray(convertJsonToArray((JSONArray) value));
-            } else if (value instanceof  Boolean) {
-                array.pushBoolean((Boolean) value);
-            } else if (value instanceof  Integer) {
-                array.pushInt((Integer) value);
-            } else if (value instanceof  Double) {
-                array.pushDouble((Double) value);
-            } else if (value instanceof String)  {
-                array.pushString((String) value);
-            } else {
-                array.pushString(value.toString());
+            try {
+                Object value = jsonArray.get(i);
+                if (value instanceof JSONObject) {
+                    array.pushMap(convertJsonToMap((JSONObject) value));
+                } else if (value instanceof  JSONArray) {
+                    array.pushArray(convertJsonToArray((JSONArray) value));
+                } else if (value instanceof  Boolean) {
+                    array.pushBoolean((Boolean) value);
+                } else if (value instanceof  Integer) {
+                    array.pushInt((Integer) value);
+                } else if (value instanceof  Double) {
+                    array.pushDouble((Double) value);
+                } else if (value instanceof String)  {
+                    array.pushString((String) value);
+                } else {
+                    array.pushString(value.toString());
+                }
+            } catch (JSONException e) {
+
             }
         }
         return array;
     }
 
-    public static JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
+    public static JSONObject convertMapToJson(ReadableMap readableMap){
         JSONObject object = new JSONObject();
+        if (readableMap == null) {
+            return null;
+        }
         ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        if (!iterator.hasNextKey()) {
+            return null;
+        }
         while (iterator.hasNextKey()) {
             String key = iterator.nextKey();
-            switch (readableMap.getType(key)) {
+            ReadableType readableType = readableMap.getType(key);
+
+            try {
+                switch (readableType) {
                 case Null:
                     object.put(key, JSONObject.NULL);
                     break;
@@ -90,14 +124,23 @@ public class ReactNativeJson {
                     object.put(key, convertArrayToJson(readableMap.getArray(key)));
                     break;
             }
+            } catch (JSONException e) {
+
+            }
         }
         return object;
     }
 
-    public static JSONArray convertArrayToJson(ReadableArray readableArray) throws JSONException {
+    public static JSONArray convertArrayToJson(ReadableArray readableArray){
         JSONArray array = new JSONArray();
+        if (readableArray == null){
+            return null;
+        }
+
         for (int i = 0; i < readableArray.size(); i++) {
-            switch (readableArray.getType(i)) {
+            ReadableType readableType = readableArray.getType(i);
+            try {
+                switch (readableType) {
                 case Null:
                     break;
                 case Boolean:
@@ -115,7 +158,11 @@ public class ReactNativeJson {
                 case Array:
                     array.put(convertArrayToJson(readableArray.getArray(i)));
                     break;
+                }
+            } catch (JSONException e) {
+
             }
+            
         }
         return array;
     }
